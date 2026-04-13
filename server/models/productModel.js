efc05f356fcdb4d7b35d41786d476f57f372d234
Nityagopaljena98 <nityagjena@hotmail.com>
2026-04-13 17:16:29 +0530

    fix(product-schema): correct timestamps typo and add validations for price and rating also add brand

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema(
-  {
-    productName: { type: String, required: true },
-    price: { type: Number, required: true },
-    rating: { type: Number, required: true },
-    description: { type: String, required: true },
-    category: { type: String, enum: ['electronics', 'fashion'] , required:true},
-  },
-  { timeStamps: true },
+    {
+        productName: { type: String, required: true },
+        brand: { type: String, required: true },
+        price: { type: Number, required: true, min: 0 },
+        rating: { type: Number, required: true, min: 0, max: 5 },
+        description: { type: String, required: true },
+        category: { type: String, enum: ['electronics', 'fashion'], required: true },
+    },
+    { timestamps: true },
 );
 
-const Product = mongoose.model('PRODUCT', productSchema);
+const Product = mongoose.model('Product', productSchema);
 module.exports = Product;
