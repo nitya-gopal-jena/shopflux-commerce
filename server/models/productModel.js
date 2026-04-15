0ff03cfc739e723016b3a0db8d069f6b3d47e64b
Nityagopaljena98 <nityagjena@hotmail.com>
2026-04-15 20:52:02 +0530

    feat(schema): add description, category, and subcategory fields

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,15 +1,35 @@
 const mongoose = require('mongoose');
 
+const subCategories = {
+  electronics: ['mobile', 'laptop', 'tv'],
+  fashion: ['mens', 'womens', 'kids'],
+};
+
 const productSchema = mongoose.Schema(
-    {
-        productName: { type: String, required: true },
-        brand: { type: String, required: true },
-        price: { type: Number, required: true, min: 0 },
-        rating: { type: Number, required: true, min: 0, max: 5 },
-        description: { type: String, required: true },
-        category: { type: String, enum: ['electronics', 'fashion'], required: true },
+  {
+    productName: { type: String, required: true },
+    brand: { type: String, required: true },
+    price: { type: Number, required: true, min: 0 },
+    rating: { type: Number, required: true, min: 0, max: 5 },
+    description: { type: String, required: true },
+    category: {
+      type: String,
+      enum: Object.keys(subCategories),
+      required: true,
     },
-    { timestamps: true },
+
+    subCategory: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return subCategories[this.category].includes(value);
+        },
+        message: 'Invalid subcategory for selected category',
+      },
+    },
+  },
+  { timestamps: true },
 );
 
 const Product = mongoose.model('Product', productSchema);
