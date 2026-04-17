5a28710fea7f49733d387ac0e48029e34a200472
Nityagopaljena98 <nityagjena@hotmail.com>
2026-04-17 14:16:07 +0530

    replaced the home component from layout to pages folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar/Navbar';
-import Home from './components/layout/home/Home';
+import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Service from './components/pages/Service';
 import About from './components/pages/About';
